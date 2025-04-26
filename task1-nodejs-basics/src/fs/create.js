b446872182465601c834e9efd135fa90dfb622f0
GeorgeShani <shanidze.geo2004@gmail.com>
2025-04-26 18:01:22 +0400

    feat: implement create.js at src/fs

--- task1-nodejs-basics/src/fs/create.js
+++ task1-nodejs-basics/src/fs/create.js
@@ -1,5 +1,17 @@
+import { promises as fs } from "fs";
+import { join } from "path";
+
 const create = async () => {
-    // Write your code here 
+  const filePath = join("src", "fs", "files", "fresh.txt");
+  const content = "I am fresh and young";
+
+  try {
+    await fs.writeFile(filePath, content, { flag: "wx" });
+    console.log("File created successfully.");
+  } catch (error) {
+    console.error(error.message); // Show real error
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
