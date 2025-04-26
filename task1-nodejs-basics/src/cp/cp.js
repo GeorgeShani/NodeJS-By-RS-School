97f82e31a27a305f41434f808879ee93a4f3af48
GeorgeShani <shanidze.geo2004@gmail.com>
2025-04-27 00:01:34 +0400

    feat: implement cp.js at src/cp

--- task1-nodejs-basics/src/cp/cp.js
+++ task1-nodejs-basics/src/cp/cp.js
@@ -1,6 +1,35 @@
+import { spawn } from "child_process";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  // Construct full path to script.js
+  const scriptPath = path.resolve(__dirname, "files", "script.js");
+
+  // Spawn child process
+  const childProcess = spawn("node", [scriptPath, ...args], {
+    stdio: ["pipe", "pipe", "pipe", "ipc"], // Setup IPC channels
+  });
+
+  // Forward stdin from mster process to child process
+  process.stdin.pipe(childProcess.stdin);
+
+  // Forward stdout from child process to master process
+  childProcess.stdout.pipe(process.stdout);
+
+  // Handle errors
+  childProcess.stderr.on("data", (data) => {
+    console.error(`Child process error: ${data}`);
+  });
+
+  // Log when child process exits
+  childProcess.on("exit", (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["Hello", "from", "VS Code", "to", "RS", "School"]);
