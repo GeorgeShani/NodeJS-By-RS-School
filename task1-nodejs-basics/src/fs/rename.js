695bce7228df186db3d4a1a59c6eb5de74aa0494
GeorgeShani <shanidze.geo2004@gmail.com>
2025-04-26 18:14:33 +0400

    feat: implement rename.js at src/fs

--- task1-nodejs-basics/src/fs/rename.js
+++ task1-nodejs-basics/src/fs/rename.js
@@ -1,5 +1,30 @@
+import { promises as fs } from "fs";
+import { join } from "path";
+
 const rename = async () => {
-    // Write your code here 
+  const oldPath = join("src", "fs", "files", "wrongFilename.txt");
+  const newPath = join("src", "fs", "files", "properFilename.md");
+
+  try {
+    // Check if 'wrongFilename.txt' exists
+    await fs.access(oldPath);
+
+    // Check id 'properFilename.md already exists'
+    try {
+      await fs.access(newPath);
+      // If access succeeds, it means 'properFilename.md' exists, so we throw error
+      throw new Error("properFilename.md already exists");
+    } catch {
+      // If access fails, it means 'properFilename.md' does not exist — good, we proceed
+    }
+
+    // Perform renaming
+    await fs.rename(oldPath, newPath);
+    console.log("File renamed successfully.");
+  } catch (error) {
+    console.error(error.message);
+    throw new Error("FS operation failed");
+  }
 };
 
 await rename();
