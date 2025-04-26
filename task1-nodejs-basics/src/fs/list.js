fa9e35af605d94e6e6e1c56ba9a01cc3ffc8d905
GeorgeShani <shanidze.geo2004@gmail.com>
2025-04-26 18:23:02 +0400

    feat: implement list.js at src/fs

--- task1-nodejs-basics/src/fs/list.js
+++ task1-nodejs-basics/src/fs/list.js
@@ -1,5 +1,21 @@
+import { promises as fs } from "fs";
+import { join } from "path";
+
 const list = async () => {
-    // Write your code here 
+  const folderPath = join("src", "fs", "files");
+
+  try {
+    // Check if 'files' folder exists
+    await fs.access(folderPath);
+
+    // Read all filenames
+    const files = await fs.readdir(folderPath);
+
+    console.log(files);
+  } catch (error) {
+    console.error(error.message);
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
