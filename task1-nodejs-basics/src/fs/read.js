bbd5f45b6f31d483dd9972fa7dccbfbbc0ca1eee
GeorgeShani <shanidze.geo2004@gmail.com>
2025-04-26 18:26:14 +0400

    feat: implement read.js at src/fs

--- task1-nodejs-basics/src/fs/read.js
+++ task1-nodejs-basics/src/fs/read.js
@@ -1,5 +1,21 @@
+import { promises as fs } from "fs";
+import { join } from "path";
+
 const read = async () => {
-    // Write your code here 
+  const filePath = join("src", "fs", "files", "fileToRead.txt");
+  
+  try {
+    // Check if 'fileToRead.txt' exists
+    await fs.access(filePath);
+
+    // Read file content
+    const content = await fs.readFile(filePath, "utf-8");
+
+    console.log(content);
+  } catch (error) {
+    console.error(error.message);
+    throw new Error("FS operation failed");
+  }
 };
 
 await read();
