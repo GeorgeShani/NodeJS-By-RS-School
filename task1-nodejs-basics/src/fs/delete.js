7c767c99418b115d3e89919eb54ebf5fcd0479f3
GeorgeShani <shanidze.geo2004@gmail.com>
2025-04-26 18:20:51 +0400

    feat: implement delete.js at src/fs

--- task1-nodejs-basics/src/fs/delete.js
+++ task1-nodejs-basics/src/fs/delete.js
@@ -1,5 +1,20 @@
+import { promises as fs } from "fs";
+import { join } from "path";
+
 const remove = async () => {
-    // Write your code here 
+  const filePath = join("src", "fs", "files", "fileToRemove.txt");
+
+  try {
+    // Check if 'fileToRemove.txt' exists
+    await fs.access(filePath);
+
+    // Delete the file
+    await fs.unlink(filePath);
+    console.log("File deleted successfully.");
+  } catch (error) {
+    console.error(error.message);
+    throw new Error("FS operation failed");
+  }
 };
 
 await remove();
