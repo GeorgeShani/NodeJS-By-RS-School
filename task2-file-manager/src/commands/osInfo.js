df348b1ddf295bd7042e3ab3879c89069a807270
GeorgeShani <shanidze.geo2004@gmail.com>
2025-05-02 12:08:08 +0400

    feat: implement os info related commands

---
+++ task2-file-manager/src/commands/osInfo.js
@@ -0,0 +1,34 @@
+import os from "os";
+
+// Get and display OS information
+const getOSInfo = (flag) => {
+  try {
+    switch (flag) {
+      case "--EOL":
+        console.log(JSON.stringify(os.EOL));
+        break;
+      case "--cpus":
+        const cpus = os.cpus();
+        console.log(`Total CPUs: ${cpus.length}`);
+        cpus.forEach((cpu, index) => {
+          console.log(`CPU ${index + 1}: ${cpu.model} (${(cpu.speed / 1000).toFixed(2)} GHz)`);
+        });
+        break;
+      case "--homedir":
+        console.log(os.homedir());
+        break;
+      case "--username":
+        console.log(os.userInfo().username);
+        break;
+      case "--architecture":
+        console.log(process.arch);
+        break;
+      default:
+        throw new Error("Invalid OS info parameter");
+    }
+  } catch (error) {
+    throw new Error("Failed to get OS info");
+  }
+};
+
+export { getOSInfo };
