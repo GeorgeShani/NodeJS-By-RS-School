dee72be87b61fa511aba0295aab32832bf5d758e
GeorgeShani <shanidze.geo2004@gmail.com>
2025-05-02 19:05:10 +0400

    feat: initialize command line interface for "file manager"

--- task2-file-manager/src/index.js
+++ task2-file-manager/src/index.js
@@ -1,1 +1,19 @@
-// Main code will be executed here
+import { parseArgs, printCurrentDir } from "./utils.js";
+import { setupCLI } from "./cli.js";
+import os from "os";
+
+// Initialize file manager
+const initFileManager = () => {
+  const { username } = parseArgs();
+
+  // Set initial working directory to user's home directory
+  process.chdir(os.homedir());
+
+  console.log(`Welcome to the File Manager, ${username}!`);
+  printCurrentDir();
+
+  // Set up CLI interface
+  setupCLI(username);
+};
+
+initFileManager();
